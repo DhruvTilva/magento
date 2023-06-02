dad25da9f3cf16a09cc6fb4835348a41460b5b23
DhruvTilva <tilvadhruv8@gmail.com>
2023-06-02 10:14:02 +0530

    vendor-address-new-name-DT-space

---
+++ app/code/local/DT/Vendor/Model/Resource/Vendor/Address/Collection.php
@@ -0,0 +1,10 @@
+<?php
+
+class DT_Vendor_Model_Resource_Vendor_Address_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
+    protected function _construct()
+    {
+        //address collection
+        $this->_init('vendor/vendor_address');
+    }
+}
