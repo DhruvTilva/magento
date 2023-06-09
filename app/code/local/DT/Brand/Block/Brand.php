e7d6d19f9cd5a6319320a3c7b9d32a7b56f9df96
DhruvTilva <tilvadhruv8@gmail.com>
2023-06-09 11:45:46 +0530

    brand-module-upgrade-0.3.0-frontend-banner-saw successfully

---
+++ app/code/local/DT/Brand/Block/Brand.php
@@ -0,0 +1,50 @@
+<?php
+
+class DT_Brand_Block_Brand extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getBrands()
+    {
+        $brands = Mage::getModel('brand/brand')->getCollection();
+        $brands->addFieldToFilter('status',1);
+        $brands->setOrder('sort_order','ASC');
+        return $brands;
+    }
+
+    public function getBrand()
+    {
+        $brandId = $this->getRequest()->getParam('brand_id');
+        $brand = Mage::getModel('brand/brand')->load($brandId);
+        return $brand;
+    }
+
+    public function getProductsByBrand()
+    {
+
+        $brandAttributeCode = 'brand';
+        $brandAttribute = Mage::getSingleton('eav/config')->getAttribute('catalog_product', $brandAttributeCode);
+
+        $brandValue = $this->getRequest()->getParam('brand_id');
+        $productCollection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToFilter($brandAttributeCode, $brandValue)
+            ->getAllIds();
+
+        $products = Mage::getModel('catalog/product')->getCollection()
+            ->addIdFilter($productCollection)
+            ->addAttributeToSelect('*');
+        return $products;
+    }
+
+    public function getProductUrl($product)
+    {
+        $productId = $product->getId(); 
+        $rewrite = Mage::getModel('core/url_rewrite')->load($productId,'product_id');
+        $requestPath = $rewrite->getRequestPath();
+        return $requestPath;
+    }
+
+}
